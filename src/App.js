b38a407f3855354ef171955d3269faa6316ab2de
Domokos Balazs <domokos.balazs@gloobus.it>
2023-11-08 14:35:33 +0200

    Files added Your branch is up to date with 'origin/main'.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Helmet } from 'react-helmet';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+    render() {
+        return (
+            <>
+                <Helmet>
+                    <title>Domokos Balazs - Web & Game Developer</title>
+                    <meta name="description" content="Showcasing the web and game development portfolio of Domokos Balazs. Explore innovative projects and creative designs." />
+                    <meta name="keywords" content="Domokos Balazs, Web Developer, Game Developer, Portfolio, Web Development, Game Design" />
+                    <meta property="og:title" content="Domokos Balazs - Web & Game Developer" />
+                    <meta property="og:description" content="Discover the portfolio of Domokos Balazs, where web development meets game design. Learn about my projects, skills, and professional journey." />
+                    <meta property="og:image" content="https://domokosbalazs.ro/images/ogimage.jpg" />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:url" content="https://domokosbalazs.ro/" />
+                    <meta name="twitter:card" content="summary_large_image" />
+                    <meta name="twitter:title" content="Domokos Balazs - Web & Game Developer" />
+                    <meta name="twitter:description" content="Portfolio of Domokos Balazs: From web applications to immersive games, discover a unique blend of technology and creativity." />
+                    <meta name="twitter:image" content="https://domokosbalazs.ro/images/twitterimage.jpg" />
+                    <link rel="canonical" href="https://domokosbalazs.ro/" />
+                    {/* Additional tags as needed */}
+                </Helmet>
+                <header>
+                    <h1>Welcome to <a href="http://www.domokosbalazs.ro" target="_blank">domokosbalazs.ro</a></h1>
+                </header>
 
+                <main>
+                    <div className="conten retro-tv torsion corner-effect">
+                        <div className="noise">
+                            <canvas id="noiseCanvas"></canvas>
+                        </div>
+
+                        <p>🎮 Aspiring sci-fi universe creator by day, 🕹️ pixel art enthusiast by night. I code, I craft, and I bring worlds to life. If I'm not debugging, you'll find me dodging virtual asteroids or plotting epic space odysseys. Let's navigate the stars together! 🚀
+                            <br /><br />
+                            Connect with me on 🌐<a href="https://www.linkedin.com/in/domokos-balazs/" target="_blank">LinkedIn</a> to explore more dimensions of my journey!
+                            <br />
+                            Check out my game creations on 🎮<a href="https://domokos.itch.io/" target="_blank">itch.io</a> for some virtual adventures!
+                            <br />
+                            Join me on ▶️<a href="https://youtube.com/@DomokosBalazs" target="_blank">YouTube</a> for behind-the-scenes game development and intergalactic fun!
+                            <br />
+                            Tweet along with me on 🐦<a href="https://twitter.com/DomokosBalazs" target="_blank">Twitter</a> for cosmic thoughts and game dev banter!</p>
+                    </div>
+                </main>
+                <footer>
+                    <p>&copy; All rights reserved domokosbalazs.ro</p>
+                </footer>
+            </>
+        );
+    }
+}
 export default App;
